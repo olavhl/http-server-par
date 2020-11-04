5cb3045ae27e3c94176ba4069c5d263f17c429d0
Olav Hartwedt Larsen <ohalar@gmail.com>
2020-11-04 13:54:09 +0100

    Pushing changes in httpclient

--- src/main/java/no/kristiania/http/HttpClient.java
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -4,20 +4,34 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class HttpClient {
-    public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("urlecho.appspot.com", 80);
 
-        String request = "GET /echo?status=200&body=HelloWorld\r\n" +
-                "Host: urlecho.appspot.com\r\n\r\n";
+
+    public HttpClient(String hostname, int port, String requestTarget) throws IOException {
+        Socket socket = new Socket(hostname, port);
+
+        String request = "GET " + requestTarget + " HTTP/1.1\r\n" +
+                "Host: " + hostname + "\r\n\r\n";
 
         socket.getOutputStream().write(request.getBytes());
 
-        int c;
 
+        StringBuilder line = new StringBuilder();
+        int c;
         while ((c = socket.getInputStream().read()) != -1) {
-            System.out.print((char)c);
+            if (c == '\n') {
+                break;
+            }
+            line.append((char)c);
         }
+        System.out.println(line);
     }
 
+    public static void main(String[] args) throws IOException {
+       String hostname = "urlecho.appspot.com";
+       String requestTarget = "/echo?status=200&body=HelloWorld";
+       int port = 80;
+
+       new HttpClient(hostname, port, requestTarget);
+    }
 
 }
