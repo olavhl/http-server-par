45847bbf4e6632babe60e91b86c83dac8142ea1c
Olav Hartwedt Larsen <ohalar@gmail.com>
2020-11-04 13:01:37 +0100

    Initial commit

---
+++ src/main/java/no/kristiania/http/QueryString.java
@@ -0,0 +1,23 @@
+package no.kristiania.http;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class QueryString {
+
+
+    private final Map<String, String> parameters = new HashMap<>();
+
+    public QueryString(String queryString) {
+        for (String parameter : queryString.split("&")) {
+            int equalPos = parameter.indexOf('=');
+            String key = parameter.substring(0, equalPos);
+            String value = parameter.substring(equalPos+1);
+            parameters.put(key, value);
+        }
+    }
+
+    public String getParameter(String key) {
+        return parameters.get(key);
+    }
+}
---
+++ src/test/java/no/kristiania/http/QueryStringTest.java
@@ -0,0 +1,21 @@
+package no.kristiania.http;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class QueryStringTest {
+
+    @Test
+    public void shouldRetrieveParameter() {
+        QueryString queryString = new QueryString("status=200");
+        assertEquals("200", queryString.getParameter("status"));
+    }
+
+    @Test
+    public void shouldRetrieveMultipleParameters() {
+        QueryString queryString = new QueryString("status=404&body=HelloThere");
+        assertEquals("404", queryString.getParameter("status"));
+        assertEquals("HelloThere", queryString.getParameter("body"));
+    }
+}
